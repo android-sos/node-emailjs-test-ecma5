947a08bb1d828e2d8fa127a3830f64d79e77596c
Samuel Ospina <samuel_carlo77@hotmail.com>
2016-05-06 10:31:07 -0430

    Inicio

---
+++ index.js
@@ -0,0 +1,15 @@
+var email   = require("./path/to/emailjs/email");
+var server  = email.server.connect({
+   user:    "samuel.ospina36@gmail.com", 
+   password:"S1234567s.", 
+   host:    "smtp.gmail.com", 
+   ssl:     true
+});
+
+// send the message and get a callback with an error or details of the message that was sent
+server.send({
+   text:    "i hope this works", 
+   from:    "samuel <samuel.ospina36@gmail.com>", 
+   to:      "someone <samuel.ospina36@gmail.com>",
+   subject: "testing emailjs"
+}, function(err, message) { console.log(err || message); });
