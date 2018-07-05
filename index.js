f8ca94a13cdb7430671b60db7ee9658d47e276a1
Samuel Ospina <samuel_carlo77@hotmail.com>
2018-07-05 10:22:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var server  = email.server.connect({
 // send the message and get a callback with an error or details of the message that was sent
 server.send({
    text:    "i hope this works", 
-   from:    "samuel <samuel.ospina36@gmail.com>", 
-   to:      "someone <samuel.ospina36@gmail.com>",
+   from:    "samuel <**************>", 
+   to:      "someone <*************>",
    cc: "",
    subject: "testing emailjs"
 }, function(err, message) { console.log(err || message); });
